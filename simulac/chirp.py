f18d7f246ac7c18abae0d50a31dfe1cec1aaf30c
Ignacio Martinez Vazquez <ignamv@gmail.com>
2013-08-29 20:55:46 +0100

    Calculate MOSAIC peak of MOSAIC'ed autocorrelation

---
+++ simulac/chirp.py
@@ -0,0 +1,15 @@
+import numpy as np
+from peaks import find_min
+from mosaic import mosaic
+
+def mosaicpeak(mosaic):
+    return np.max(mosaic[find_min(mosaic)])/np.max(mosaic)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Measure MOSAIC peak')
+    parser.add_argument('input', type=argparse.FileType('r'))
+    cfg = parser.parse_args()
+
+    tau, mosaic = np.loadtxt(cfg.input, unpack=True)
+    print(mosaicpeak(mosaic))
