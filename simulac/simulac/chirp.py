822b75f15935f455b4407a94dbb7319aa71dd760
Ignacio Martinez Vazquez <ignamv@gmail.com>
2013-09-05 15:10:58 -0300

    Calculate MOSAIC peak

--- simulac/simulac/chirp.py
+++ simulac/simulac/chirp.py
@@ -1,9 +1,19 @@
+#-- encoding: utf-8 --
 import numpy as np
 from .peaks import find_min
 from .mosaic import mosaic
+from .generate_ac import center_ac
 
-def mosaicPeak(mosaic):
-    return np.max(mosaic[find_min(mosaic)])/np.max(mosaic)
+def mosaicPeak(mos):
+    """Calculate MOSAIC peak of a MOSAIC-processed autocorrelation
+
+    Estimate a pulse's chirp using the MOSAIC technique [1].
+
+    [1]T. Hirayama y M. Sheik-Bahae, «Real-time chirp diagnostic for ultrashort laser pulses», Optics letters, vol. 27, n.º 10, pp. 860-862, 2002.
+    """
+
+    mos = center_ac(mos)
+    return np.max(mos[find_min(mos)])/np.max(mos)
 
 if __name__ == '__main__':
     import argparse
