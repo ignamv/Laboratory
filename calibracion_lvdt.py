eec7042c394e094e7673f7112688a66177fcd361
Ignacio Martinez Vazquez <ignamv@gmail.com>
2014-02-17 12:13:31 -0300

    Plot LVDT calibration

---
+++ calibracion_lvdt.py
@@ -0,0 +1,21 @@
+#--encoding: utf-8 --
+import numpy as np
+from matplotlib import pyplot as plt
+
+calibration_file = 'C:\\ignacio\\mediciones\\lvdt\\calibracion lvdt.csv'
+
+calib_data = np.loadtxt(calibration_file, delimiter=',')
+# Saco polinomio para distancia en funcion de tensión
+calibration = np.polyfit(calib_data[:,1],
+                         calib_data[:,0],
+                         1)
+
+plt.xlabel('Voltage [V]')
+plt.ylabel('Distance [mm]')
+plt.plot(calib_data[:,1],calib_data[:,0], '-x', label='Measured')
+fit = np.polyval(calibration, calib_data[:,1])
+plt.plot(calib_data[:,1],fit, '-x', label='Fit')
+plt.legend()
+plt.show()
+
+print(calibration)
